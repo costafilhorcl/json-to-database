14ae262d0a702ab8c7716cfa8a0f30365ee2168a
Roberto <costafilhorcl@gmail.com>
2020-04-12 11:21:01 -0300

    vai

--- json_parse.py
+++ json_parse.py
@@ -7,10 +7,5 @@ class JSONParser:
         #new development
         #new development without rebase
         #vai dar pau
-<<<<<<< HEAD
         #mais alteracoes. dessa vez vai pra rebase
         #vamos fazer um rebase
-=======
-        #vamos fazer um rebase
-
->>>>>>> 48535501d8e290edb1369c87073bcc6e3167855b
